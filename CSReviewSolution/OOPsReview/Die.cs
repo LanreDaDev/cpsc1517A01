3779ec5058e010c17313d99dd196092e2c589450
oadebayo4 <46577127+oadebayo4@users.noreply.github.com>
2019-01-16 14:51:14 -0700

    Update Die.cs

--- CSReviewSolution/OOPsReview/Die.cs
+++ CSReviewSolution/OOPsReview/Die.cs
@@ -13,6 +13,13 @@ namespace OOPsReview
 
     public class Die
     {
+        //use the System.Random class for my random generator
+        // the instance will be statuc so all instances of Die will use
+        // the same generator
+        // the instance of random will be created when the first instance 
+        // of Die is create and be destroyed when the last instance
+        // of die is destroyed
+        public static Random _rnd = new Random();
         // Data Members '_Name'
         // This is the physical storage area for data values 
         // These  are usually private
@@ -36,7 +43,7 @@ namespace OOPsReview
 
 
         //Fully Implemented Property
-        public int Side
+        public int Sides
         {
             get
             {
@@ -50,21 +57,104 @@ namespace OOPsReview
                 // The value is stored in a key word: value
                 // the datatype of value is the return datatype of the property
                 //validatoin can be done on the value 
-                if(value >= 6 && value <=20)
+                if (value >= 6 && value <= 20)
                 {
                     _Sides = value;
+                    Roll();
                 }
                 else
                 {
                     throw new Exception("Die cannot have " + value.ToString() + "sides. Die can have 6 to 20 sides.");
                 }
-                
+
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+
+            }
+            set
+            {
+                //value == "" test? empty string 
+                // value == null test? no string exists
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception("Cannot be an empty string");
+
+                }
+                else
+                {
+                    _Color = value;
+                }
+
             }
         }
 
 
 
         //Auto Implemented Property
+        // there is no defined data member for this property
+        // the system creates an internal storage area of the 
+        //   property datatype and manages the area for your code.
+        // typically this typ[e of property is commonly used when no 
+        // validation is required for the data
+        public int FaceValue { get; private set; }
+
+        // Constructors are optional
+        // if you do not include a contructor for your class then when an
+        // instance of the class is created, the system will assign 
+        // the normal default values of that datatype to the 
+        // appropriate data member.
+
+        // if your declare a constructor within your class definition
+        // then, you are responsible for the class
+        // constuctors are NOT called directly by the user of the class
+        // the constructor will be called when you ask the system to create
+        // an instance of the class
+
+
+        // syntax public classname([list of parameters]) {Coding body}
+
+
+        // default constructor 
+        // this is similar to having a system constructor
+        // it has no parameters
+        public Die()
+        {
+            // typically this constructor will have no code, that is, use 
+            // the system defaults
+
+            // you could assign your own defaults
+            _Sides = 6;
+            Color = "White";
+            Roll();
+        }
+
+        // greedy constructor
+        // takes in value for each of your data members/ auto properties
+        // this allows the outside "User" wants to specify the values at time
+        // of creation for the instance.
+        public Die(int sides, string color)
+        {
+            Sides = sides;
+            Color = color;
+            Roll();
+        }
+        // Behaviours (Methods)
+        //a method will allow the user to
+        //a) manipulate the data of the instance
+        //b) use the data of the instance to generate some other information
+
+        public void Roll()
+        {
+            // will generate a random value for Facevalue
+            FaceValue = _rnd.Next(1, Sides + 1);
+        }
 
     }
 }
+
